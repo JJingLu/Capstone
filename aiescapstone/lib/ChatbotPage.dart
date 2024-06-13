408454959e08df52a1bc7729da0f873bd3dd90e8
Andrew ZHONG <zhonghaosong2001@126.com>
2024-06-13 21:01:31 +0800

    update chatbot

--- aiescapstone/lib/ChatbotPage.dart
+++ aiescapstone/lib/ChatbotPage.dart
@@ -7,12 +7,12 @@ import 'ChatbotWrapper.dart';
   });
 */
 
-class ChatPage extends StatefulWidget {
+class ChatbotPage extends StatefulWidget {
   @override
   _ChatPageState createState() => _ChatPageState();
 }
 
-class _ChatPageState extends State<ChatPage> {
+class _ChatPageState extends State<ChatbotPage> {
   TextEditingController _textEditingController = TextEditingController();
   List<String> _chatMessages = [];
 
@@ -106,23 +106,5 @@ class ChatbotTesterPage extends StatelessWidget {
       ),
       home: ChatbotPage(),
     );
-  }
-}
-
-class ChatbotPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          // TRY THIS: Try changing the color here to a specific color (to
-          // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-          // change color while the other colors stay the same.
-          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-          // Here we take the value from the MyHomePage object that was created by
-          // the App.build method, and use it to set our appbar title.
-          title: const Text("Misogyny Chatbot"),
-        ),
-        backgroundColor: Colors.black87,
-        body: ChatPage());
   }
 }
